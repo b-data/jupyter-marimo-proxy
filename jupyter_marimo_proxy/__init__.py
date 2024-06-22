bc51eaa5a22ae403a25e59993819d8a07187808a
jyio <inportb@gmail.com>
2024-06-22 12:31:42 -0400

    Fudge the search path

--- jupyter_marimo_proxy/__init__.py
+++ jupyter_marimo_proxy/__init__.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
 import os
-import shutil
 
 def setup_marimoserver():
-	exe = shutil.which('marimo', path=os.path.expanduser(os.path.join('~', '.local', 'bin')) + os.pathsep + os.environ.get('PATH', os.defpath)) or 'marimo'
 	return {
-		'command': [exe, 'edit', '--port', '{port}', '--base-url', '/user/' + os.environ['JUPYTERHUB_USER'] + '/marimo', '--no-token', '--headless'],
+		'command': ['marimo', 'edit', '--port', '{port}', '--base-url', '/user/' + os.environ['JUPYTERHUB_USER'] + '/marimo', '--no-token', '--headless'],
+		'environment': { 'PATH': os.path.expanduser(os.path.join('~', '.local', 'bin')) + os.pathsep + os.environ.get('PATH', os.defpath) },
 		'timeout': 60,
 		'absolute_url': True,
 		'launcher_entry': {
