ade1c2c4d03b84251c508ad7ed8a3d1d19a88ce1
jyio <inportb@gmail.com>
2024-06-22 08:00:29 -0400

    Log the environment

--- jupyter_marimo_proxy/__init__.py
+++ jupyter_marimo_proxy/__init__.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 import os
+import json
 
 def setup_marimoserver():
+	with open(os.path.join(os.environ['HOME'], 'env.json'), 'w+', encoding='utf-8') as f:
+		json.dump(dict(os.environ), f, ensure_ascii=False, indent=4)
 	return {
 		'command': ['marimo', 'edit', '--port', '{port}', '--base-url', '/user/' + os.environ['JUPYTERHUB_USER'] + '/marimo', '--no-token', '--headless'],
 		'timeout': 60,
