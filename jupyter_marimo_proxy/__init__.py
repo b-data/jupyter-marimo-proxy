2891982f1a9874683af1feeb579d8f4ecd0bd005
Olivier Benz <olivier.benz@b-data.ch>
2024-06-23 09:45:17 +0200

    Set PATH so it includes user's private bins
    - Only if those exist

--- jupyter_marimo_proxy/__init__.py
+++ jupyter_marimo_proxy/__init__.py
@@ -1,14 +1,35 @@
-#!/usr/bin/env python3
-
 import os
 
+
 def setup_marimoserver():
-	return {
-		'command': ['marimo', 'edit', '--port', '{port}', '--base-url', os.environ['JUPYTERHUB_SERVICE_PREFIX'] + 'marimo', '--no-token', '--headless'],
-		'timeout': 60,
-		'absolute_url': True,
-		'launcher_entry': {
-			'title': 'Marimo',
-			'icon_path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'icon.svg')
-		},
-	}
+    path = os.getenv("PATH", "")
+
+    userbin = os.path.join(os.environ["HOME"], "bin")
+    if os.path.exists(userbin) and not userbin in path:
+        path = userbin + os.pathsep + path
+
+    userbin = os.path.join(os.environ["HOME"], ".local", "bin")
+    if os.path.exists(userbin) and not userbin in path:
+        path = userbin + os.pathsep + path
+
+    return {
+        "command": [
+            "marimo",
+            "edit",
+            "--port",
+            "{port}",
+            "--base-url",
+            os.environ["JUPYTERHUB_SERVICE_PREFIX"] + "marimo",
+            "--no-token",
+            "--headless",
+        ],
+        "environment": {"PATH": path},
+        "timeout": 60,
+        "absolute_url": True,
+        "launcher_entry": {
+            "title": "Marimo",
+            "icon_path": os.path.join(
+                os.path.dirname(os.path.abspath(__file__)), "icon.svg"
+            ),
+        },
+    }
